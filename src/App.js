e42c030f2beea8e58267a63e0fc4bcec13f49951
ThopsD <74445064+thops1925@users.noreply.github.com>
2021-11-03 02:09:21 +0800

    Link add

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
 import { Navbar } from './components';
+import './App.css';
 
 function App() {
   return (
@@ -9,7 +10,26 @@ function App() {
       <div className="navbar">
         <Navbar />
       </div>
-      <div className="main"></div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Switch>
+              <Route exact path="/">
+                <Homepage />
+              </Route>
+              <Route exact path="/exchanges">
+                <Exchanges />
+              </Route>
+              <Route exact path="/cryptocurrencies">
+                <Cryptocurrencies />
+              </Route>
+              <Route exact path="/">
+                <Homepage />
+              </Route>
+            </Switch>
+          </div>
+        </Layout>
+      </div>
       <div className="footer"></div>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,8 @@ import { Button, Menu, Typography, Avatar } from 'antd';
 import { Link } from 'react-router-dom';
 import {
   HomeOutlined,
-  MoneyCollectionOutlined,
-  BulboutOutlined,
+  MoneyCollectOutlined,
+  BulbOutlined,
   FundOutlined,
   MenuOutlined,
 } from '@ant-design/icons';
@@ -14,13 +14,29 @@ function Navbar() {
   return (
     <div className="nav-container">
       <div className="logo-container">
-        <Avatar src={icons} size="large">
-          <Typography.Title level={2} className="logo">
-            <Link to="/">Cryptoverse</Link>
-          </Typography.Title>
-          {/* <Button></Button> */}
-        </Avatar>
+        <Avatar src={icons} size="large" />
+        <Typography.Title level={2} className="logo">
+          <Link to="/">Cryptoverse</Link>
+        </Typography.Title>
+        {/* <Button></Button> */}
       </div>
+      <Menu theme="dark">
+        <Menu.Item icon={<HomeOutlined />}>
+          <Link to="/">Home</Link>
+        </Menu.Item>
+        <Menu.Item icon={<FundOutlined />}>
+          <Link to="/cryptocurrency">Cryptocurrency</Link>
+        </Menu.Item>
+        <Menu.Item icon={<MoneyCollectOutlined />}>
+          <Link to="/exchangers">Exchanges</Link>
+        </Menu.Item>
+        <Menu.Item icon={<BulbOutlined />}>
+          <Link to="/news">News</Link>
+        </Menu.Item>
+        <Menu.Item icons={<MenuOutlined />}>
+          <Link to="/">Home</Link>
+        </Menu.Item>
+      </Menu>
     </div>
   );
 }
